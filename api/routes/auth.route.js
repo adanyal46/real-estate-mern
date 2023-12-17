1bec6d7eabff0e00d622fa35b75b0e7974ed788b
Danyal Mehmood <adanyal46@gamil.com>
2023-12-17 20:23:44 +0500

    create a middleware for error

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
-import { signIn } from '../controllers/auth.controller.js'
+import { signUp } from '../controllers/auth.controller.js'
 const router = express.Router()
 
-router.post('/sign-in', signIn)
+router.post('/sign-up', signUp)
 export default router
