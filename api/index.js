0b071e3d756ca42a6b4221f648f7778361c9fa18
Danyal Mehmood <adanyal46@gamil.com>
2023-12-17 19:49:49 +0500

    create a sign up api route

--- api/index.js
+++ api/index.js
@@ -2,8 +2,10 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.routes.js'
+import authRouter from './routes/auth.route.js'
 dotenv.config()
 const app = express()
+app.use(express.json({ limit: '5mb' }))
 mongoose
 	.connect(process.env.MONGO)
 	.then(res => {
@@ -13,6 +15,7 @@ mongoose
 		console.log('Mongo Db error')
 	})
 app.use('/api/user', userRouter)
+app.use('/api/auth', authRouter)
 app.listen(3000, () => {
 	console.log('Server is running on port 3000')
 })
