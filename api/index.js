177296f518bde4299224d0ad83ce818f7a981979
Danyal Mehmood <adanyal46@gamil.com>
2023-12-24 14:59:37 +0500

    api profile update and verify user method

--- api/index.js
+++ api/index.js
@@ -3,9 +3,11 @@ import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRouter from './routes/user.routes.js'
 import authRouter from './routes/auth.route.js'
+import cookieParser from 'cookie-parser'
 dotenv.config()
 const app = express()
 app.use(express.json({ limit: '5mb' }))
+app.use(cookieParser())
 mongoose
 	.connect(process.env.MONGO)
 	.then(res => {
