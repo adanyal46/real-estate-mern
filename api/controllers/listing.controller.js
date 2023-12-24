eb5ca30cd95410557a73fccb8b5e92b7fac14419
Danyal Mehmood <adanyal46@gamil.com>
2023-12-24 20:55:52 +0500

    view listing api and delete listing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,9 +1,32 @@
 import Listing from '../models/listing.model.js'
+import { errorHandler } from '../utils/error.js'
 
 export const createListing = async (req, res, next) => {
 	try {
 		const listing = await Listing.create(req.body)
 		return res.status(201).json(listing)
+	} catch (error) {
+		next(error)
+	}
+}
+export const getListing = async (req, res, next) => {
+	try {
+		const listing = await Listing.findById(req.params.id)
+		if (!listing) {
+			return next(errorHandler(401, 'Listing not found'))
+		}
+		return res.status(201).json(listing)
+	} catch (error) {
+		next(error)
+	}
+}
+export const deleteListing = async (req, res, next) => {
+	try {
+		const listing = await Listing.findByIdAndDelete(req.params.id)
+		if (!listing) {
+			return next(errorHandler(401, 'Listing not found'))
+		}
+		return res.status(201).json('Listing Deleted Successfully')
 	} catch (error) {
 		next(error)
 	}
--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -59,7 +59,8 @@ export const deleteUser = async (req, res, next) => {
 	}
 }
 export const getUserListings = async (req, res, next) => {
-	if (req.user.id !== req.params.id) {
+	console.log(req.user)
+	if (req.user._id !== req.params.id) {
 		try {
 			const listing = await Listing.find({ userRef: req.params.id })
 			res.status(200).json(listing)
@@ -68,13 +69,5 @@ export const getUserListings = async (req, res, next) => {
 		}
 	} else {
 		return next(errorHandler(401, 'You can only view our own listing!'))
-	}
-
-	try {
-		await User.findByIdAndDelete(req.params.id)
-		res.clearCookie('access_token')
-		res.status(200).json('User has been deleted!')
-	} catch (error) {
-		next(error)
 	}
 }
